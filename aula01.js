bfad70073de7a08837ae791b9c14f45dea611023
LFZ <147456764+LuixFelipes@users.noreply.github.com>
2024-06-03 22:00:39 -0300

    Exercicio01

--- aula01.js
+++ aula01.js
@@ -1,3 +1,14 @@
-console.log("Como se tornar um programdor Full Stack")
-console.log("luiz.felipes1997@gmail.com") 
-console.log("felipes.luiz1997@gmail.com")
+let nome
+let sobrenome
+let campoEstudo
+let anoNascimento
+let idade
+
+nome = prompt("Qual seu nome:")
+sobrenome = prompt("Qual seu sobrenome:")
+campoEstudo = prompt("Qual seu campo de estudo:")
+anoNascimento = prompt("Qual seu ano de nascimento")
+
+idade = 2024 - anoNascimento
+
+confirm("Nome: ", nome," ", sobrenome, "\nIdade: ", idade, "\nCampo de Estudo: ",campoEstudo)
